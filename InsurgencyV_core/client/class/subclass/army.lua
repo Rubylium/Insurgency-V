cd0f85af78995c1f5e891ab1b7d11ea72b55cf1c
Rubylium <alexisoko0@gmail.com>
2020-07-04 18:59:29 +0200

    Adding some army functions

--- InsurgencyV_core/client/class/subclass/army.lua
+++ InsurgencyV_core/client/class/subclass/army.lua
@@ -2,4 +2,22 @@ armypeds = {
     {label = "Blackops 1", model = "s_m_y_blackops_01"},
     {label = "Blackops 2", model = "s_m_y_blackops_02"},
     {label = "Blackops 3", model = "s_m_y_blackops_03"},
+    {label = "Marine 1", model = "s_m_m_marine_01"},
+    {label = "Marine 2", model = "s_m_y_marine_01"},
+    {label = "Marine 3", model = "s_m_y_marine_03"},
 }
+
+
+local ArmyWeapons = {
+    "weapon_carbinerifle",
+    "weapon_combatpdw",
+    "weapon_pistol_mk2",
+    "weapon_flaregun",
+}
+
+function JoinArmy()
+    player.camp = "army",
+    for k,v in pairs(ArmyWeapons) do
+        GiveWeaponToPed(player.ped, GetHashKey(v), 255, 0, 1)
+    end
+end
