4871676c23000610444ecf842ed4ce223194a166
Rubylium <alexisoko0@gmail.com>
2020-07-04 17:29:52 +0200

    Adding few army peds

---
+++ InsurgencyV_core/client/class/subclass/army.lua
@@ -0,0 +1,5 @@
+armypeds = {
+    {label = "Blackops 1", model = "s_m_y_blackops_01"},
+    {label = "Blackops 2", model = "s_m_y_blackops_02"},
+    {label = "Blackops 3", model = "s_m_y_blackops_03"},
+}
