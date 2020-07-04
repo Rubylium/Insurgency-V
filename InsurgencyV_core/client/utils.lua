95377568fc249f46cb551c283726bee1d3da06d0
Rubylium <alexisoko0@gmail.com>
2020-07-04 20:50:25 +0200

    Update utils.lua

--- InsurgencyV_core/client/utils.lua
+++ InsurgencyV_core/client/utils.lua
@@ -5,3 +5,16 @@ function LoadModel(model)
     RequestModel(model)
     while not HasModelLoaded(model) do Wait(0) end
 end
+
+
+function ShowHelpNotification(msg, thisFrame)
+	AddTextEntry('HelpNotif', msg)
+	DisplayHelpTextThisFrame('HelpNotif', false)
+end
+
+
+function SpawnVeh(model)
+    LoadModel(model)
+    local veh = CreateVehicle(GetHashKey(model), player.coords, GetEntityHeading(player.ped), 1, 0)
+    SetPedIntoVehicle(player.pPed, veh, -1)
+end
