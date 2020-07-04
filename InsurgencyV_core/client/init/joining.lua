e9e9825e44cab327dc0ef0bdcd935da73f46ba21
Rubylium <alexisoko0@gmail.com>
2020-07-04 15:07:35 +0200

    Initial Init

---
+++ InsurgencyV_core/client/init/joining.lua
@@ -0,0 +1,15 @@
+Citizen.CreateThread(function()
+    while not NetworkIsSessionActive() do Wait(1) end
+    initCinematic()
+end)
+
+
+
+function initCinematic()
+    SetNuiFocus(true, true)
+    TriggerEvent("xsound:stateSound", "cinematic", "https://www.youtube.com/watch?v=Jme5hYgXbNY&list=PL953Es57AbWdc4oqOC5HWLWGD7Xvk1qYd&index=1", 0.2, true)
+
+
+
+    -- Display menu
+end
---
+++ InsurgencyV_core/fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'adamant'
+games { 'gta5' };
+
+client_scripts {
+    "src/client/RMenu.lua",
+    "src/client/menu/RageUI.lua",
+    "src/client/menu/Menu.lua",
+    "src/client/menu/MenuController.lua",
+
+    "src/client/components/*.lua",
+
+    "src/client/menu/elements/*.lua",
+
+    "src/client/menu/items/*.lua",
+
+    "src/client/menu/panels/*.lua",
+
+    "src/client/menu/windows/*.lua",
+
+}
+
+client_scripts {
+     --'tests/rmenu/basic.lua',
+}
+
+
