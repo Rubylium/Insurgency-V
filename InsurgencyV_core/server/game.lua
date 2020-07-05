ec3dd5b114adddfb116a277cedf84228e89f4b00
Rubylium <alexisoko0@gmail.com>
2020-07-05 19:04:59 +0200

    Update game.lua

--- InsurgencyV_core/server/game.lua
+++ InsurgencyV_core/server/game.lua
@@ -1,15 +1,31 @@
 
+resitance = 0
+army = 0
 
-RegisterCommand("reset", function(source, args, rawCommand)
-    if source == 0 then
-        local vehs = GetAllVehicles()
-        for k,v in pairs(vehs) do
-            DeleteEntityYes(v)
-        end
-    end
-end, true)
+points = {
+    army = 0,
+    resitance = 0,
+}
 
 
 function DeleteEntityYes(net)
     Citizen.InvokeNative(`DELETE_ENTITY` & 0xFFFFFFFF, net)
 end
+
+
+
+
+
+Citizen.CreateThread(function()
+    while true do
+        for k,v in pairs(captureZone) do
+            if v.team == "army" then
+                points.army = points.army + 5
+            elseif v.item == "resistance" then
+                points.resitance = points.resitance + 5
+            end
+        end
+        TriggerClientEvent("V:Sync", -1, resitance, army, points)
+        Wait(5000)
+    end
+end)
