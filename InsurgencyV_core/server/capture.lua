88438b940d09d32f12f1bddff2123fabfa2db23a
Rubylium <alexisoko0@gmail.com>
2020-07-05 15:01:00 +0200

    Update capture.lua

--- InsurgencyV_core/server/capture.lua
+++ InsurgencyV_core/server/capture.lua
@@ -1,13 +1,27 @@
 
 
 local captureZone = {
-    {label = "Test zone", team = "Neutral"},
+    {label = "Silo", team = "Neutral"},
+    {label = "Crossroad", team = "Neutral"},
+    {label = "Color montain", team = "Neutral"},
+    {label = "Sandy Shores",team = "Neutral"},
+    {label = "Yellow Jack", team = "Neutral"},
+    {label = "Mine", team = "Neutral"},
+    {label = "Montain house", team = "Neutral"},
+    {label = "Fuel farm",  team = "Neutral"},
+    {label = "Church", team = "Neutral"},
+    {label = "Vacation house", team = "Neutral"},
+    {label = "Deposit", team = "Neutral"},
+    {label = "Camp", team = "Neutral"},
+    {label = "Liquor market", team = "Neutral"},
 }
 
 
 
 RegisterNetEvent("V:CapturePoint")
 AddEventHandler("V:CapturePoint", function(id, team)
+    print(id, camp)
+    print(type(id), type(camp))
     if team ~= "army" and team ~= "resistance" then
         return
     end
