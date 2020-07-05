091419eeed1a0332231df9c469720dac64b444c9
Rubylium <alexisoko0@gmail.com>
2020-07-05 11:51:04 +0200

    Update capture.lua

---
+++ InsurgencyV_core/server/capture.lua
@@ -0,0 +1,18 @@
+
+
+local captureZone = {
+    {label = "", team = "Neutral"}
+}
+
+
+
+RegisterNetEvent("V:CapturePoint")
+AddEventHandler("V:CapturePoint", function(id, team)
+    if team ~= "army" and team ~= "resistance" then
+        return
+    end
+    if captureZone[id] ~= nil then
+        captureZone[id].team = team
+        TriggerClientEvent("V:ZoneCaptured", -1, captureZone[id].label, captureZone[id].team)
+    end
+end)
