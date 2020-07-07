35b71e83a8c5aa7bdbe442f129a008a1b216c61d
Rubylium <alexisoko0@gmail.com>
2020-07-07 12:19:13 +0200

    Update army.lua

--- InsurgencyV_core/client/class/subclass/army.lua
+++ InsurgencyV_core/client/class/subclass/army.lua
@@ -1,18 +1,14 @@
 armypeds = {
-    {label = "Blackops 1", model = "s_m_y_blackops_01"},
-    {label = "Blackops 2", model = "s_m_y_blackops_02"},
-    {label = "Blackops 3", model = "s_m_y_blackops_03"},
-    {label = "Marine 1", model = "s_m_m_marine_01"},
-    {label = "Marine 2", model = "s_m_y_marine_01"},
     {label = "Marine 3", model = "s_m_y_marine_03"},
 }
 
 
 local ArmyWeapons = {
-    "weapon_carbinerifle",
     "weapon_combatpdw",
     "weapon_pistol_mk2",
     "weapon_flaregun",
+    "weapon_grenade",
+    "weapon_carbinerifle",
 }
 
 function JoinArmy(music)
