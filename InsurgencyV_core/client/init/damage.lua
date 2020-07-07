5981da9edc9d3b9dac2b030336720cf034d7d899
Rubylium <alexisoko0@gmail.com>
2020-07-07 22:45:59 +0200

    Update damage.lua

--- InsurgencyV_core/client/init/damage.lua
+++ InsurgencyV_core/client/init/damage.lua
@@ -1,11 +1,11 @@
 
 
 local weapons = {
-    {weap = "weapon_pistol", multi = 2.0},
-    {weap = "weapon_pistol_mk2", multi = 2.0},
-    {weap = "weapon_combatpdw", multi = 2.4},
-    {weap = "weapon_assaultrifle", multi = 3.4},
-    {weap = "weapon_carbinerifle", multi = 3.4},
+    {weap = "weapon_pistol", multi = 0.1},
+    {weap = "weapon_pistol_mk2", multi = 5.0},
+    {weap = "weapon_combatpdw", multi = 13.4},
+    {weap = "weapon_assaultrifle", multi = 15.0},
+    {weap = "weapon_carbinerifle", multi = 15.0},
     {weap = "weapon_rpg", multi = 50.0},
     {weap = "weapon_stickybomb", multi = 20.0},
 }
