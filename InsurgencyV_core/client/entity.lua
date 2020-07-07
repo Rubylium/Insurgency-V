7c60aeed9cf4ba4dc2f3a2d6a4753d7709689c7e
Rubylium <alexisoko0@gmail.com>
2020-07-07 22:45:57 +0200

    Update entity.lua

--- InsurgencyV_core/client/entity.lua
+++ InsurgencyV_core/client/entity.lua
@@ -68,8 +68,9 @@ function EnumeratePickups()
 	return EnumerateEntities(FindFirstPickup, FindNextPickup, EndFindPickup)
 end
 
-
+local blips = {}
 Citizen.CreateThread(function()
+	while player == nil do Wait(100) end
 	while true do
 		local particles = {}
 		RequestNamedPtfxAsset("scr_trevor3")
@@ -86,19 +87,49 @@ Citizen.CreateThread(function()
 		for v in EnumerateVehicles() do
 			if GetEntityHealth(v) < 100 then
 				local co = GetEntityCoords(v)
-				print('Adding particle for veh '..v)
+
 				UseParticleFxAssetNextCall("scr_trevor3")
 				local particle = StartParticleFxLoopedAtCoord("scr_trev3_trailer_plume", co.x, co.y, co.z+0.4, 1.0, 1.0, 100.0, 0.7, false, false)
+				table.insert(particles, particle)
+
 				UseParticleFxAssetNextCall("scr_agencyheistb")
 				local particle = StartParticleFxLoopedAtCoord("scr_env_agency3b_smoke", co.x, co.y, co.z+0.4, 1.0, 1.0, 100.0, 0.7, false, false)
 				table.insert(particles, particle)
 			end
 		end
 
+		for k,v in pairs(blips) do
+			RemoveBlip(v)
+		end
+
+		if player.camp == "army" then
+			for v in EnumeratePeds() do
+				if IsPedAPlayer(v) then
+					if GetEntityModel(v) == GetHashKey("s_m_y_marine_03") then
+						local blip = AddBlipForEntity(v)
+						SetBlipScale(blip, 0.65)
+						SetBlipColour(blip, 67)
+						table.insert(blips, blip)
+					end
+				end
+			end
+		elseif player.camp == "resistance" then
+			for v in EnumeratePeds() do
+				if IsPedAPlayer(v) then
+					if GetEntityModel(v) == GetHashKey("s_m_y_blackops_01") then
+						local blip = AddBlipForEntity(v)
+						SetBlipScale(blip, 0.65)
+						SetBlipColour(blip, 67)
+						table.insert(blips, blip)
+					end
+				end
+			end
+		end
 
 		Wait(1500)
 		for k,v in pairs(particles) do
 			StopParticleFxLooped(v)
+			
 			table.remove(particles, k)
 		end
 	end
