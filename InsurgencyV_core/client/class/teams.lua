51bba1b362503fe55a3f01e19949eb9b6da90d99
Rubylium <alexisoko0@gmail.com>
2020-07-08 21:23:25 +0200

    Adding team kill msg + punishment

--- InsurgencyV_core/client/class/teams.lua
+++ InsurgencyV_core/client/class/teams.lua
@@ -13,3 +13,14 @@
 --function JoinResistanceTeam()
 --    SetPedRelationshipGroupHash(PlayerPedId(), 'resistance')
 --end
+
+local teamkill = 0
+RegisterNetEvent("V:TeamKill")
+AddEventHandler("V:TeamKill", function()
+    teamkill = teamkill + 1
+    PlaySoundFrontend(-1, "TIMER_STOP", "HUD_MINI_GAME_SOUNDSET", 1)
+    ShowPopupWarning("~r~TEAMKILL!\n~s~Teamkill is not allowed and will result as a perma ban if abused.")
+    if teamkill > 2 then
+        SetEntityHealth(GetPlayerPed(-1), 0)
+    end
+end)
